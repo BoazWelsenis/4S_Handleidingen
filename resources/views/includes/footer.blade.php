767b027bbd41d61d1d312651a22a80fc5a40174e
BoazWelsenis <D281029@edu.rocwb.nl>
2021-09-21 15:11:46 +0200

    Footer uitbreiden (Ticket 2B)
    De footer is uitgebreid door een 3 verschillendie sections toe te voegen. Namelijk: Over ons, contactgegevens en social media. Er moest dummiegegevens gebruikt worden. Dit is ook gebeurt.

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,6 +1,25 @@
 
 <footer>
 	© {{ __('misc.copyright') }}
+  
+  <div class="footer-grid">
+    <div class="over-ons">
+      <h2>Over ons</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </div>
+    <div class="contact-gegevens">
+      <h2>Contactgegevens</h2>
+      <p>John Doe</p>
+      <p>06-123456</p>
+      <p>test@test.com</p>
+    </div>
+    <div class="social-links">
+      <h2>Social Media</h2>
+      <p><a href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram"></i></a></p>
+      <p><a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook"></i></a></p>
+      <p><a href="https://twitter.com/" target="_blank"><i class="fab fa-twitter"></i></a></p>
+    </div>
+  </div>
 </footer>
 
 
--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -7,5 +7,6 @@
 <meta name="google-site-verification" content="Fdb3h6i9VSbRysXMwdteA4E2tZnedlygCcIyJBADqKo" />
 
 <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
 
 <title>Download your manual: Free user guides for all brands and devices!</title>
