c435a926a2876f6930c07b65f6324f42a1ab3142
BoazWelsenis <D281029@edu.rocwb.nl>
2021-09-21 15:24:37 +0200

    Footer Ticket 2B aangepast
    Er zijn nog wat kleine aanpassingen gedaan wat betreft de footer. De layout is namelijk wat aangepast, zodat de richtlijnen / eisen van ticket 1b worden opgevolgd (qua uitlijning).

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -5,19 +5,24 @@
   <div class="footer-grid">
     <div class="over-ons">
       <h2>Over ons</h2>
+      
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
     </div>
     <div class="contact-gegevens">
       <h2>Contactgegevens</h2>
+
       <p>John Doe</p>
       <p>06-123456</p>
       <p>test@test.com</p>
     </div>
     <div class="social-links">
       <h2>Social Media</h2>
-      <p><a href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram"></i></a></p>
-      <p><a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook"></i></a></p>
-      <p><a href="https://twitter.com/" target="_blank"><i class="fab fa-twitter"></i></a></p>
+
+      <div class="social-icon-links">
+        <p><a href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram"></i></a></p>
+        <p><a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook"></i></a></p>
+        <p><a href="https://twitter.com/" target="_blank"><i class="fab fa-twitter"></i></a></p>
+      </div>
     </div>
   </div>
 </footer>
